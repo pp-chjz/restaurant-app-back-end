6aa7541909b34d69ce0a25d9a345089b874d50c2
pp_chjz <pirapat.j@ku.th>
2023-02-10 01:19:54 +0700

    create MenuCollection and Resource

--- app/Http/Resources/MenuResource.php
+++ app/Http/Resources/MenuResource.php
@@ -17,8 +17,19 @@ class MenuResource extends JsonResource
         //return ค่าเดิม
         // return parent::toArray($request);
 
+        $ingredients = $this->ingredients;
+
         return[
             'id' => $this->id,
+            'catagories' => $this->catagories,
+            'sort_menu' => $this->sort_menu,
+            'name_ENG' => $this->name_ENG,
+            'name_TH' => $this->name_TH,
+            'menu_status' => $this->menu_status,
+            'price' => $this->price,
+            'size' => $this->size,
+            'comment' => $this->comment,
+            'ingredient' => $ingredients,
         ];
     }
 }
