92324bb8e2948d216070de31954928a766b308e9
pp_chjz <pirapat.j@ku.th>
2023-02-21 02:03:02 +0700

    create menu_order_table

---
+++ database/migrations/2023_02_21_014919_create_menu_order_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_oreder', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Order::class);
+            $table->foreignIdFor(\App\Models\Menu::class);
+
+            $table->foreignIdFor(\App\Models\Tablenumber::class);
+            $table->enum('status',array('complete','not complete'));
+            $table->longText('comment');
+            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_oreder');
+    }
+}
